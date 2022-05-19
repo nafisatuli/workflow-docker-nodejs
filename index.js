9040664713fc4e06c1dc8f0833f40ca41dd70ab8
Nafisa Hasan Tuli <nafisahasan98@gmail.com>
2022-05-19 11:46:33 +0600

    setting up express.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h3>Hello World</h3>")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
